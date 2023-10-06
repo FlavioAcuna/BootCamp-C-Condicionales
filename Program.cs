085bd37309dff9e40a53f8fb772950fea02cce86
FlavioAcuña <flavio.acuna.t@gmail.com>
2023-10-06 10:54:29 -0300

    Changes to be committed: 	new file:   Condicionales.csproj 	new file:   Program.cs 	new file:   bin/Debug/net7.0/Condicionales.deps.json 	new file:   bin/Debug/net7.0/Condicionales.dll 	new file:   bin/Debug/net7.0/Condicionales.exe 	new file:   bin/Debug/net7.0/Condicionales.pdb 	new file:   bin/Debug/net7.0/Condicionales.runtimeconfig.json 	new file:   obj/Condicionales.csproj.nuget.dgspec.json 	new file:   obj/Condicionales.csproj.nuget.g.props 	new file:   obj/Condicionales.csproj.nuget.g.targets 	new file:   obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   obj/Debug/net7.0/Condicionales.AssemblyInfo.cs 	new file:   obj/Debug/net7.0/Condicionales.AssemblyInfoInputs.cache 	new file:   obj/Debug/net7.0/Condicionales.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net7.0/Condicionales.GlobalUsings.g.cs 	new file:   obj/Debug/net7.0/Condicionales.assets.cache 	new file:   obj/Debug/net7.0/Condicionales.csproj.AssemblyReference.cache 	new file:   obj/Debug/net7.0/Condicionales.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net7.0/Condicionales.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net7.0/Condicionales.dll 	new file:   obj/Debug/net7.0/Condicionales.genruntimeconfig.cache 	new file:   obj/Debug/net7.0/Condicionales.pdb 	new file:   obj/Debug/net7.0/apphost.exe 	new file:   obj/Debug/net7.0/ref/Condicionales.dll 	new file:   obj/Debug/net7.0/refint/Condicionales.dll 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+condicionales
+> < >= <= != == !
+operadores 
+|| &&
+
+
+
+*/
+
+string nombre = "Alex";
+int edad = 16;
+
+if (edad >= 18)
+{
+    Console.WriteLine(nombre + " ya puedes manejar");
+}
+else
+{
+    int anios = 18 - edad;
+    Console.WriteLine(nombre + " te faltan " + anios + " años para poder manejar");
+    Console.WriteLine($"{nombre} te faltan {anios} años para poder manejar");
+}
+
+
+char num = '8';
+int num2 = 56; //Mirar codigo ASCII
+
+if (num==num2)
+{
+Console.WriteLine("Son iguales");
+}else
+{
+    Console.WriteLine("Son diferentes");
+}
